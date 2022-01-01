df38971f762cfd949d9513c375fc8d92fc7ae0b9
Nazar <wolfter12@gmail.com>
2022-01-01 20:52:20 +0200

    add redirect if user is authenticated

--- pages/auth/index.jsx
+++ pages/auth/index.jsx
@@ -1,6 +1,32 @@
+import { useState, useEffect } from "react";
+import { getSession } from "next-auth/client";
+import { useRouter } from "next/router";
+
 import AuthForm from "../../components/auth/auth-form";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    async function checkSession() {
+      const session = await getSession();
+
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    }
+
+    checkSession();
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return <AuthForm />;
 }
 
