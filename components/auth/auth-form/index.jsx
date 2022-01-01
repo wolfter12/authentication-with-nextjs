799bdeb978fa0c4cd67c4125aae14c18e12c9d45
Nazar <wolfter12@gmail.com>
2022-01-01 17:27:44 +0200

    add fetching signup form

--- components/auth/auth-form/index.jsx
+++ components/auth/auth-form/index.jsx
@@ -1,24 +1,94 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
 import classes from "./auth-form.module.css";
+
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response?.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+
+  return data;
+}
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const [authFormValues, setAuthFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = authFormValues;
+
+  const onChangeHandler = ({ target: { name, value } }) => {
+    setAuthFormValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    // TODO: add email and password validation
+
+    if (isLogin) {
+      // log user in
+    } else {
+      try {
+        const result = await createUser(email, password);
+        setAuthFormValues({
+          email: "",
+          password: "",
+        });
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={onChangeHandler}
+            required
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={onChangeHandler}
+            required
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
