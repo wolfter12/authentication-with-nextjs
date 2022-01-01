40cbf073b85bc8a8595b24ca66c24f27d8abc326
Nazar <wolfter12@gmail.com>
2022-01-01 20:42:09 +0200

    replace client side session checking with server side

--- components/profile/user-profile/index.jsx
+++ components/profile/user-profile/index.jsx
@@ -1,30 +1,8 @@
-import { useState, useEffect } from "react";
-import { getSession } from "next-auth/client";
-
 import ProfileForm from "../profile-form";
 
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function updateSession() {
-      const session = await getSession();
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
-    }
-
-    updateSession();
-  }, []);
-
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
-
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
