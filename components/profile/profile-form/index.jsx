c59f22babfe500060ded15d034b632003e3068c3
Nazar <wolfter12@gmail.com>
2022-01-01 22:33:48 +0200

    add change password logic frontend

--- components/profile/profile-form/index.jsx
+++ components/profile/profile-form/index.jsx
@@ -1,15 +1,61 @@
+import { useState } from "react";
+
 import classes from "./profile-form.module.css";
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const [formInputValues, setFormInputValues] = useState({
+    newPassword: "Qwerty78",
+    oldPassword: "Qwerty789",
+  });
+
+  const { newPassword, oldPassword } = formInputValues;
+
+  function changeHandler({ target: { name, value } }) {
+    setFormInputValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    const { changePasswordHandler } = props;
+
+    // TODO: add validation
+
+    const normalizedNewPassword = newPassword.trim();
+    const normalizedOldPassword = oldPassword.trim();
+
+    changePasswordHandler({
+      newPassword: normalizedNewPassword,
+      oldPassword: normalizedOldPassword,
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" />
+        <label htmlFor="newPassword">New Password</label>
+        <input
+          type="password"
+          name="newPassword"
+          id="newPassword"
+          value={newPassword}
+          onChange={changeHandler}
+          required
+        />
       </div>
       <div className={classes.control}>
-        <label htmlFor="old-password">Old Password</label>
-        <input type="password" id="old-password" />
+        <label htmlFor="oldPassword">Old Password</label>
+        <input
+          type="password"
+          name="oldPassword"
+          id="oldPassword"
+          value={oldPassword}
+          onChange={changeHandler}
+          required
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- components/profile/user-profile/index.jsx
+++ components/profile/user-profile/index.jsx
@@ -3,10 +3,30 @@ import ProfileForm from "../profile-form";
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
+  async function changePasswordHandler({ newPassword, oldPassword }) {
+    console.info("submitHandler", { newPassword, oldPassword });
+
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify({
+        newPassword,
+        oldPassword,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    // TODO: add notification
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm changePasswordHandler={changePasswordHandler} />
     </section>
   );
 }
